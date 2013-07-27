732aa454a9fd73587da20dd5f8bc8e14e5739e22
Hardy Cherry <hardyc3@gmail.com>
2013-07-26 22:06:33 -0600

    Initial commit of project and libraries

---
+++ TDRacer/src/TDRacerMain.cpp
@@ -0,0 +1,19 @@
+#include "SDL.h"
+
+int main(int argc, char* args[]) 
+{
+	//Start SDL 
+	SDL_Init( SDL_INIT_EVERYTHING ); 
+	
+	SDL_Window* screen = NULL;
+	//Set up screen 
+	screen = SDL_CreateWindow("TDRacer", 100, 100, 640, 480, SDL_SWSURFACE );
+
+	//Pause 
+	SDL_Delay( 2000 );
+
+	//Quit SDL 
+	SDL_Quit(); 
+	
+	return 0; 
+}
